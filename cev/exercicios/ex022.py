2d3dd3148d69fc333a30611c2f9937083bc71b96
JoaoVitorJuliao <defoX@outlook.com.br>
2020-10-31 10:47:45 -0300

    Manipulação de Texto e Exercícios

---
+++ cev/exercicios/ex022.py
@@ -0,0 +1,1 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre: O nome com todas as letras maiúsculas, o nome com todas minúsculas, quantas letras ao todo (sem considerar espaços) e quantas letras tem o primeiro nome.
