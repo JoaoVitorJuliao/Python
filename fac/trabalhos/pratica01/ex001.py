379d788a0cbbe84067fd4bcd9030c1c9f7f339d2
JoaoVitorJuliao <defoX@outlook.com.br>
2020-12-08 14:39:52 -0300

    Exclusão de alguns exercícios

--- fac/trabalhos/pratica01/ex001.py
+++
@@ -1,5 +0,0 @@
-# 1. Faça um programa que dado o valor da temperatura em graus FAHRENHEIT, calcular e escrever o valor da temperatura em graus CELSIUS. Fórmula: C = 5/9 * (F - 32)
-
-c = float(input('Digite uma temperatura em Celsius? '))
-
-print(f'A temperatura {c :.1f}°C, equivale a {c * 9 / 5 + 32 :.1f}°F.')
