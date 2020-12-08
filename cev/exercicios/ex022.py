776b1fdaa9b38f97c153632a5f5a1ddd56bab626
JoaoVitorJuliao <defoX@outlook.com.br>
2020-12-08 16:56:00 -0300

    Exercício 22 pronto

--- cev/exercicios/ex022.py
+++ cev/exercicios/ex022.py
@@ -1,1 +1,8 @@
 # Crie um programa que leia o nome completo de uma pessoa e mostre: O nome com todas as letras maiúsculas, o nome com todas minúsculas, quantas letras ao todo (sem considerar espaços) e quantas letras tem o primeiro nome.
+
+nome = str(input('Digite seu nome completo: ')).strip()
+
+print(f'O seu nome em maiúsculas é {nome.upper()}.')
+print(f'O seu nome em minúsculas é {nome.lower()}.')
+print('O seu nome tem {} letras.' .format(len(nome) - nome.count(' ')))
+print('O seu primeiro nome tem {} letras.' .format(nome.find(' ')))
