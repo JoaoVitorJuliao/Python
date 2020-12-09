8a818b685844cd2c95f916a7a7530ef0c5ae38ae
JoaoVitorJuliao <defoX@outlook.com.br>
2020-12-09 20:03:53 -0300

    Conclusão dos exercícios 23, 24 e 25.

--- cev/exercicios/ex024.py
+++ cev/exercicios/ex024.py
@@ -1,1 +1,10 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
+
+c = str(input('Digite o nome de uma cidade: '))
+
+cidade = c.strip().upper()
+
+if cidade[:5] == 'SANTO':
+    print('A sua cidade começa com a palavra Santo.')
+else:
+    print('A sua cidade não começa com a palavra Santo.')
--- cev/exercicios/ex025.py
+++ cev/exercicios/ex025.py
@@ -1,1 +1,10 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+n = str(input('Digite o seu nome: ')).strip().upper()
+
+nome = n.find('SILVA')
+
+if nome >= 0:
+    print('O seu nome tem a palavra Silva.')
+else:
+    print('O seu nome não tem a palavra Silva.')
