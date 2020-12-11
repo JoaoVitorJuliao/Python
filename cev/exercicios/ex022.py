51a643a847810aaaa0c301c80cfadf27b704f375
JoaoVitorJuliao <defoX@outlook.com.br>
2020-12-11 08:26:45 -0300

    OBS: Quando começar com '', diferenciar com "". Quando começar com "", diferenciar com ''.

--- cev/exercicios/ex022.py
+++ cev/exercicios/ex022.py
@@ -4,5 +4,5 @@ nome = str(input('Digite seu nome completo: ')).strip()
 
 print(f'O seu nome em maiúsculas é {nome.upper()}.')
 print(f'O seu nome em minúsculas é {nome.lower()}.')
-print('O seu nome tem {} letras.' .format(len(nome) - nome.count(' ')))
-print('O seu primeiro nome tem {} letras.' .format(nome.find(' ')))
+print(f'O seu nome tem {len(nome) - nome.count(" ")} letras.')
+print(f'O seu primeiro nome tem {nome.find(" ")} letras.')
