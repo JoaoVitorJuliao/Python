4caf483b09167bacb9a1e4e2559315b889ec6641
JoaoVitorJuliao <defoX@outlook.com.br>
2020-12-11 08:39:53 -0300

    Conclusão dos exercícios 26 e 27

--- cev/exercicios/ex027.py
+++ cev/exercicios/ex027.py
@@ -1,1 +1,7 @@
 # Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente. Ex: Ana Maria de Souza; primeiro = Ana; último = Souza
+
+nome = str(input('Digite o seu nome completo: ')).strip().split()
+
+print(f"""Muito prazer em te conhecer!
+O seu primeiro nome é {nome[0]}
+O seu último nome é {nome[len(nome) - 1]}""")
