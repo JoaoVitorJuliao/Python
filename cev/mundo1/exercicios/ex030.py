0792f2fb08a16b523113fee50ed85150937eabb6
JoaoVitorJuliao <defoX@outlook.com.br>
2020-12-14 10:00:28 -0300

    Conclusão do exercício 30

--- cev/mundo1/exercicios/ex030.py
+++ cev/mundo1/exercicios/ex030.py
@@ -1,1 +1,10 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+numero = int(input('Diga-me um número qualquer: '))
+
+resto = numero % 2
+
+if resto == 0:
+    print(f'O número {numero} é PAR')
+else:
+    print(f'O número {numero} é ÍMPAR')
