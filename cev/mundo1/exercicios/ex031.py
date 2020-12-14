11d2f2ac4a3b1fc55583a427f0e0662acc252961
JoaoVitorJuliao <defoX@outlook.com.br>
2020-12-14 10:00:44 -0300

    Conclusão do exercício 31

--- cev/mundo1/exercicios/ex031.py
+++ cev/mundo1/exercicios/ex031.py
@@ -1,1 +1,9 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 para viagens mais longas.
+
+distancia = float(input('Qual a distância da sua viagem? '))
+
+print(f'Você está prestes a começar uma viagem de {distancia:.1f}Km.')
+
+preco = distancia * 0.5 if distancia <= 200 else distancia * 0.45  # Maneira simplificada
+
+print(f'E o preço da sua passagem será de R${preco:.2f}')
