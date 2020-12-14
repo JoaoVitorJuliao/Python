7643f4ec9d8c3fb8a32703fd59cb6719decb15d7
JoaoVitorJuliao <defoX@outlook.com.br>
2020-12-14 00:58:37 -0300

    Conclusão do exercício 28

--- cev/mundo1/exercicios/ex028.py
+++ cev/mundo1/exercicios/ex028.py
@@ -1,1 +1,20 @@
 # Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+from random import randint
+from time import sleep
+
+computador = randint(0, 5)
+
+print('-=-' * 20)
+print(f'Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('PENSANDO...')
+print('-=-' * 20)
+
+sleep(2)
+
+jogador = int(input('Em que número eu pensei? '))
+
+if jogador == computador:
+    print('PARABÉNS! Você conseguiu me vencer!')
+else:
+    print(f'GANHEI! Eu pensei no número {computador} e não no {jogador}!')
