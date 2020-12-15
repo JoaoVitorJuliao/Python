1ca2b1752b2ffebcdda6af4ec3e6a3a363a679f8
JoaoVitorJuliao <defoX@outlook.com.br>
2020-12-14 23:45:12 -0300

    Conclusão do exercício 34

--- cev/mundo1/exercicios/ex034.py
+++ cev/mundo1/exercicios/ex034.py
@@ -1,1 +1,9 @@
 # Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1.250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+s = float(input('Qual é o salário do funcionário? R$'))
+
+if s > 1250:
+    ns = s * 1.10
+else:
+    ns = s * 1.15
+print(f'Quem ganhava R${s:.2f} passa a ganhar R${ns:.2f} agora.')
